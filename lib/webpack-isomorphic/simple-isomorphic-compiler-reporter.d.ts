a548f1e2af84310a3336d09353dda182663d4077
Daniil Veremchuk <daniel@verydanny.com>
2019-08-28 00:45:23 -0400

    remove lib

--- lib/webpack-isomorphic/simple-isomorphic-compiler-reporter.d.ts
+++
@@ -1,5 +0,0 @@
-import { SimpleCompiler, ReporterOptionsSingleCompiler } from '../types/compiler';
-export declare function startReportingWebpackIsomorphic(compiler: SimpleCompiler, options: ReporterOptionsSingleCompiler): {
-    stop: () => void;
-    options: ReporterOptionsSingleCompiler;
-};
--- lib/webpack/simple-compiler-reporter.d.ts
+++
@@ -1,5 +0,0 @@
-import { SimpleCompiler, ReporterOptionsSingleCompiler } from '../types/compiler';
-export declare function startReportingWebpack(compiler: SimpleCompiler, options: ReporterOptionsSingleCompiler): {
-    stop: () => void;
-    options: ReporterOptionsSingleCompiler;
-};
